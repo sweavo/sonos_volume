e23807f64476257d3571217ddaf923d7f2e0ced6
Steve Carter <sweavo@gmail.com>
2020-10-29 21:36:34 +0000

    strip out a bunch more useless stuff; identify the pins
    (cherry picked from commit 6280bddfe473440af65d67e3fef1a3e71601aca3)

--- sonos_volume.ino
+++ sonos_volume.ino
@@ -18,29 +18,29 @@
 /* Sonos UPnP, v1.1 by Thomas Mittet (code@lookout.no) January 2015.    */
 /************************************************************************/
 #include <ESP8266WiFi.h>
-#include <DNSServer.h>
 #include <WiFiManager.h>
-#include <ESP8266mDNS.h>
 
 #include <SonosUPnP.h>
 #include <MicroXPath_P.h>
-
-MDNSResponder mdns;
 
 #define SERIAL_DATA_THRESHOLD_MS 500
 #define SERIAL_ERROR_TIMEOUT "E: Serial"
 #define ETHERNET_ERROR_DHCP "E: DHCP"
 #define ETHERNET_ERROR_CONNECT "E: Connect"
 
-void handleSerialRead();
 void ethConnectError();
-//EthernetClient g_ethClient;
 WiFiClient client;
 SonosUPnP g_sonos = SonosUPnP(client, ethConnectError);
 
-#define BUTTON_PIN 4    // Define pin the button is connected to
-#define ON_BOARD_LED 2  // Define pin the on-board LED is connected to
-#define RGB_G_PIN 12    // RGB Green LED
+#define PIN_BUTT_UP 4    // Define pin the button is connected to
+#define PIN_BUTT_DN 0    // Define pin the button is connected to
+
+#define PIN_LED 2  // Define pin the on-board LED is connected to
+
+#define PIN_RGB_R 15    // RGB Red LED?
+#define PIN_RGB_G 12    // RGB Green LED
+#define PIN_RGB_B 13    // RGB Blue LED?
+
 #define LDR_PIN A0      // Define the analog pin the LDR is connected to
 
 // Den
@@ -51,21 +51,20 @@ const char g_sonosID[] = "949f3e1c6e30";
 //IPAddress g_sonosIP(192, 168, 1, 48);
 //const char g_sonosID[] = "5caafd0b8104";
 
-char uri[100] = "";
-
-#include "FS.h"
-
-File f;
-
-void handleRoot();
-void handleNotFound();
-
 void setup()
 {
   // GPIO
-  pinMode(ON_BOARD_LED, OUTPUT);       // Initialize the LED_BUILTIN pin as an output
-  pinMode(BUTTON_PIN, INPUT_PULLUP);  // Initialize button pin with built-in pullup.
-  digitalWrite(ON_BOARD_LED, HIGH);    // Ensure LED is off
+  pinMode(PIN_LED, OUTPUT);
+  digitalWrite(PIN_LED, HIGH);
+  pinMode(PIN_RGB_R, OUTPUT);
+  digitalWrite(PIN_RGB_R, LOW);
+  pinMode(PIN_RGB_G, OUTPUT);
+  digitalWrite(PIN_RGB_G, LOW);
+  pinMode(PIN_RGB_B, OUTPUT);
+  digitalWrite(PIN_RGB_B, LOW);
+
+  pinMode(PIN_BUTT_DN, INPUT_PULLUP);
+  pinMode(PIN_BUTT_UP, INPUT_PULLUP);
 
   // SERIAL
   Serial.begin(115200);
@@ -74,22 +73,6 @@ void setup()
   WiFiManager wifiManager;
   wifiManager.autoConnect();
 
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-
-
-  /* File System stuff */
-
-  SPIFFS.begin();
-
-  Dir dir = SPIFFS.openDir("/");
-
-  while (dir.next()) {
-    Serial.println(dir.fileName());
-    f = dir.openFile("r");
-    Serial.println(f.size());
-    //f.close();
-  }
   Serial.println("Ready");
 }
 
@@ -126,21 +109,6 @@ void handleSerialRead()
     {
       g_sonos.pause(g_sonosIP);
     }
-    // Stop
-    else if (isCommand("st", b1, b2))
-    {
-      g_sonos.stop(g_sonosIP);
-    }
-    // Previous
-    else if (isCommand("pr", b1, b2))
-    {
-      g_sonos.skip(g_sonosIP, SONOS_DIRECTION_BACKWARD);
-    }
-    // Next
-    else if (isCommand("nx", b1, b2))
-    {
-      g_sonos.skip(g_sonosIP, SONOS_DIRECTION_FORWARD);
-    }
     // Mute On
     else if (isCommand("mu", b1, b2))
     {
@@ -160,14 +128,6 @@ void handleSerialRead()
         g_sonos.setVolume(g_sonosIP, ((b1 - '0') * 10) + (b2 - '0'));
       }
     }
-
-    else if (isCommand("ti", b1, b2))
-    {
-      Serial.println("we want the track uri");
-      TrackInfo track = g_sonos.getTrackInfo(g_sonosIP, uri, sizeof(uri));
-      Serial.println(uri);
-    }
-
   }
 }
 
@@ -175,7 +135,14 @@ void handleSerialRead()
 // Main Loop
 void loop()
 {
-  if (Serial.available() >= 2) handleSerialRead();
-  else yield();
-  digitalWrite( ON_BOARD_LED, digitalRead(BUTTON_PIN));
+  if (Serial.available() >= 2) 
+  {
+     handleSerialRead();
+  }
+  else 
+  {
+    yield();
+  }
+  digitalWrite( PIN_RGB_R,   !digitalRead( PIN_BUTT_DN ));
+  digitalWrite( PIN_RGB_B, !digitalRead( PIN_BUTT_UP ));
 }
